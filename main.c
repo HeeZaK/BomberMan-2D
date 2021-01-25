3b6fb05e7b8bf75ec5e3df9f9818f64a9a8a06e0
Rémi Ilango <45428394+sub2pewds@users.noreply.github.com>
2021-01-25 11:20:56 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <SDL.h>
+
+int main(int argc, char const *argv[]) {
+  SDL_version nb;
+  SDL_VERSION(&nb);
+
+  printf("SDL %d.%d.%d ! \n",nb.major, nb.minor, nb.patch );
+  return 0;
+}

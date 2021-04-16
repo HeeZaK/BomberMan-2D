6bd46ceacebe3642414ef365e6a13b1b06fbec4b
SimonGeslain <77961659+SimonGeslain@users.noreply.github.com>
2021-04-17 01:19:09 +0200

    Update affichage_map.c

--- src/affichage_map.c
+++ src/affichage_map.c
@@ -14,12 +14,22 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
   SDL_Window *win = NULL;
   SDL_Renderer *renderer = NULL;
 
-  SDL_Texture *bitmapFond, *bitmapMurI, *bitmapMurC, *bitmapJ1, *bitmapJ2, *bitmapJ3, *bitmapJ4, *bitmapM1;
+  SDL_Texture *bitmapFond, *bitmapMurI, *bitmapMurC;
+  SDL_Texture *bitmapJ1N, *bitmapJ1E, *bitmapJ1S, *bitmapJ1W;
+  SDL_Texture *bitmapJ2N, *bitmapJ2E, *bitmapJ2S, *bitmapJ2W;
+  SDL_Texture *bitmapJ3N, *bitmapJ3E, *bitmapJ3S, *bitmapJ3W;
+  SDL_Texture *bitmapJ4N, *bitmapJ4E, *bitmapJ4S, *bitmapJ4W;
+  SDL_Texture *bitmapM1;
   SDL_Texture *bitmapBombeJ1, *bitmapBombeJ2, *bitmapBombeJ3, *bitmapBombeJ4;
   SDL_Texture *bitmapPlusBombe, *bitmapVitesse, *bitmapPuissance, *bitmapShield;
 
   int posX = 100, posY = 100, width = 840, height = 616, w = 56, h = 56;
-  SDL_Rect rectMurI, rectMurC, rectJ1, rectJ2, rectJ3, rectJ4, rectM1;
+  SDL_Rect rectMurI, rectMurC;
+  SDL_Rect rectJ1N, rectJ1E, rectJ1S, rectJ1W;
+  SDL_Rect rectJ2N, rectJ2E, rectJ2S, rectJ2W;
+  SDL_Rect rectJ3N, rectJ3E, rectJ3S, rectJ3W;
+  SDL_Rect rectJ4N, rectJ4E, rectJ4S, rectJ4W;
+  SDL_Rect rectM1;
   SDL_Rect bombeJ1, bombeJ2, bombeJ3, bombeJ4;
   SDL_Rect plusBombe, vitesse, puissance, shield;
 
@@ -41,10 +51,29 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
   bitmapMurI = IMG_LoadTexture(renderer, "../assets/map/murincassable1.png"); //MurIncassable
   bitmapMurC = IMG_LoadTexture(renderer, "../assets/map/murcassable1.png");   //MurCassable
 
-  bitmapJ1 = IMG_LoadTexture(renderer, "../assets/bonhomme/rouge/bonhommeAvant.png");  //joueur1
-  bitmapJ2 = IMG_LoadTexture(renderer, "../assets/bonhomme/bleu/bonhommeAvant.png");   //joueur2
-  bitmapJ3 = IMG_LoadTexture(renderer, "../assets/bonhomme/jaune/bonhommeAvant.png");  //joueur3
-  bitmapJ4 = IMG_LoadTexture(renderer, "../assets/bonhomme/vert/bonhommeAvant.png");   //joueur4
+  //joueur1
+  bitmapJ1N = IMG_LoadTexture(renderer, "../assets/bonhomme/rouge/bonhommeArriere.png");
+  bitmapJ1E = IMG_LoadTexture(renderer, "../assets/bonhomme/rouge/bonhommeDroite.png");
+  bitmapJ1S = IMG_LoadTexture(renderer, "../assets/bonhomme/rouge/bonhommeAvant.png");
+  bitmapJ1W = IMG_LoadTexture(renderer, "../assets/bonhomme/rouge/bonhommeGauche.png");
+
+  //joueur2
+  bitmapJ2N = IMG_LoadTexture(renderer, "../assets/bonhomme/bleu/bonhommeArriere.png");
+  bitmapJ2E = IMG_LoadTexture(renderer, "../assets/bonhomme/bleu/bonhommeDroite.png");
+  bitmapJ2S = IMG_LoadTexture(renderer, "../assets/bonhomme/bleu/bonhommeAvant.png");
+  bitmapJ2W = IMG_LoadTexture(renderer, "../assets/bonhomme/bleu/bonhommeGauche.png");
+
+  //joueur3
+  bitmapJ3N = IMG_LoadTexture(renderer, "../assets/bonhomme/jaune/bonhommeArriere.png");
+  bitmapJ3E = IMG_LoadTexture(renderer, "../assets/bonhomme/jaune/bonhommeDroite.png");
+  bitmapJ3S = IMG_LoadTexture(renderer, "../assets/bonhomme/jaune/bonhommeAvant.png");
+  bitmapJ3W = IMG_LoadTexture(renderer, "../assets/bonhomme/jaune/bonhommeGauche.png");
+
+  //joueur4
+  bitmapJ4N = IMG_LoadTexture(renderer, "../assets/bonhomme/vert/bonhommeArriere.png");
+  bitmapJ4E = IMG_LoadTexture(renderer, "../assets/bonhomme/vert/bonhommeDroite.png");
+  bitmapJ4S = IMG_LoadTexture(renderer, "../assets/bonhomme/vert/bonhommeAvant.png");
+  bitmapJ4W = IMG_LoadTexture(renderer, "../assets/bonhomme/vert/bonhommeGauche.png");
 
   bitmapM1 = IMG_LoadTexture(renderer, "../assets/monstres/petit/monstreAvant.png");   //monstre petit
 
@@ -86,23 +115,12 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
               break;
             case SDLK_a:
               if(checkPoseBombe(&j1,jeu)){
-                //timeOutJ1 = clock();
-
-                /*for(int i=0; i<M; i++){
-                  for(int j=0; j<N; j++){
-                    if(jeu[i][j]==j1.id_bombe){
-                      x1=i;
-                      y1=j;
-                    }
-                  }
-                }*/
               }
               break;
-
-              /*temps_present=SDL_GetTicks();
-              if (temps_present > temps_reset + 1000) {
-                temps_reset = temps_present;
-              }*/
+            case SDLK_e:
+              //changer rotation
+              changerRotaJoueur(&j1);
+              break;
 
             //joueur2
             case SDLK_t:
@@ -119,17 +137,11 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
               break;
             case SDLK_r:
               if(checkPoseBombe(&j2,jeu)){
-                timeOutJ2 = clock();
-                for(int i=0; i<M; i++){
-                  for(int j=0; j<N; j++){
-                    if(jeu[i][j]==j2.id_bombe){
-                      x2=i;
-                      y2=j;
-                    }
-                  }
-                }
-                flagJ2 = 1;
               }
+              break;
+            case SDLK_y:
+              //changer rotation
+              changerRotaJoueur(&j2);
               break;
 
             //joueur3
@@ -147,17 +159,11 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
               break;
             case SDLK_u:
               if(checkPoseBombe(&j3,jeu)){
-                timeOutJ3 = clock();
-                for(int i=0; i<M; i++){
-                  for(int j=0; j<N; j++){
-                    if(jeu[i][j]==j3.id_bombe){
-                      x3=i;
-                      y3=j;
-                    }
-                  }
-                }
-                flagJ3 = 1;
               }
+              break;
+            case SDLK_o:
+              //changer rotation
+              changerRotaJoueur(&j3);
               break;
 
             //joueur4
@@ -175,17 +181,11 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
               break;
             case SDLK_RCTRL:
               if(checkPoseBombe(&j4,jeu)){
-                timeOutJ4 = clock();
-                for(int i=0; i<M; i++){
-                  for(int j=0; j<N; j++){
-                    if(jeu[i][j]==j4.id_bombe){
-                      x4=i;
-                      y4=j;
-                    }
-                  }
-                }
-                flagJ4 = 1;
               }
+              break;
+            case SDLK_RSHIFT:
+              //changer rotation
+              changerRotaJoueur(&j4);
               break;
           }
           break;
@@ -200,25 +200,32 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
         }
       }
 
-
-
-
-      /*if(flagJ1==1 && (timeOutJ1 + 2000)<clock()){
-        Detruire(&j1,&j2,&j3,&j4,x1,y1,jeu);
-        flagJ1 = 0;
+      for(int i=0; i<j2.nb_bomb; i++){
+        if((SDL_GetTicks() > j2.bombe[i].timer + 2000) && j2.bombe[i].timer!=0){
+          Detruire(&j2,&j1,&j3,&j4,i,jeu);
+          j2.bombe[i].timer=0;
+          j2.bombe[i].pos_x=0;
+          j2.bombe[i].pos_y=0;
+        }
       }
-      if(flagJ2==1 && (timeOutJ2 + 2000)<clock()){
-        Detruire(&j2,&j1,&j3,&j4,x2,y2,jeu);
-        flagJ2 = 0;
+
+      for(int i=0; i<j3.nb_bomb; i++){
+        if((SDL_GetTicks() > j3.bombe[i].timer + 2000) && j3.bombe[i].timer!=0){
+          Detruire(&j3,&j1,&j2,&j4,i,jeu);
+          j3.bombe[i].timer=0;
+          j3.bombe[i].pos_x=0;
+          j3.bombe[i].pos_y=0;
+        }
       }
-      if(flagJ3==1 && (timeOutJ3 + 2000)<clock()){
-        Detruire(&j3,&j2,&j1,&j4,x3,y3,jeu);
-        flagJ3 = 0;
+
+      for(int i=0; i<j4.nb_bomb; i++){
+        if((SDL_GetTicks() > j4.bombe[i].timer + 2000) && j4.bombe[i].timer!=0){
+          Detruire(&j4,&j1,&j3,&j2,i,jeu);
+          j4.bombe[i].timer=0;
+          j4.bombe[i].pos_x=0;
+          j4.bombe[i].pos_y=0;
+        }
       }
-      if(flagJ4==1 && (timeOutJ4 + 2000)<clock()){
-        Detruire(&j4,&j1,&j3,&j1,x4,y4,jeu);
-        flagJ4 = 0;
-      }*/
 
       SDL_RenderCopy(renderer, bitmapFond, NULL, NULL);
       for(int i=0; i<M;i++){
@@ -242,39 +249,139 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
             SDL_RenderCopy(renderer, bitmapMurC, NULL, &rectMurC);
             break;
             case -1:            //joueur1
-            rectJ1.x = w * j;
-            rectJ1.y = h * i;
-            rectJ1.w = w;
-            rectJ1.h = h;
-            SDL_RenderCopy(renderer, bitmapJ1, NULL, &rectJ1);
-            break;
+              switch(j1.direction){
+                case 'N':
+                  rectJ1N.x = w * j;
+                  rectJ1N.y = h * i;
+                  rectJ1N.w = w;
+                  rectJ1N.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ1N, NULL, &rectJ1N);
+                  break;
+                case 'E':
+                  rectJ1E.x = w * j;
+                  rectJ1E.y = h * i;
+                  rectJ1E.w = w;
+                  rectJ1E.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ1E, NULL, &rectJ1E);
+                  break;
+                case 'S':
+                  rectJ1S.x = w * j;
+                  rectJ1S.y = h * i;
+                  rectJ1S.w = w;
+                  rectJ1S.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ1S, NULL, &rectJ1S);
+                  break;
+                case 'W':
+                  rectJ1W.x = w * j;
+                  rectJ1W.y = h * i;
+                  rectJ1W.w = w;
+                  rectJ1W.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ1W, NULL, &rectJ1W);
+                  break;
+              }
+              break;
             case -2:             //joueur2
-            rectJ2.x = w * j;
-            rectJ2.y = h * i;
-            rectJ2.w = w;
-            rectJ2.h = h;
-            SDL_RenderCopy(renderer, bitmapJ2, NULL, &rectJ2);
-            break;
+              switch(j2.direction){
+                case 'N':
+                  rectJ2N.x = w * j;
+                  rectJ2N.y = h * i;
+                  rectJ2N.w = w;
+                  rectJ2N.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ2N, NULL, &rectJ2N);
+                  break;
+                case 'E':
+                  rectJ2E.x = w * j;
+                  rectJ2E.y = h * i;
+                  rectJ2E.w = w;
+                  rectJ2E.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ2E, NULL, &rectJ2E);
+                  break;
+                case 'S':
+                  rectJ2S.x = w * j;
+                  rectJ2S.y = h * i;
+                  rectJ2S.w = w;
+                  rectJ2S.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ2S, NULL, &rectJ2S);
+                  break;
+                case 'W':
+                  rectJ2W.x = w * j;
+                  rectJ2W.y = h * i;
+                  rectJ2W.w = w;
+                  rectJ2W.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ2W, NULL, &rectJ2W);
+                  break;
+              }
+              break;
             case -3:              //joueur3
-            rectJ3.x = w * j;
-            rectJ3.y = h * i;
-            rectJ3.w = w;
-            rectJ3.h = h;
-            SDL_RenderCopy(renderer, bitmapJ3, NULL, &rectJ3);
-            break;
+              switch(j3.direction){
+                case 'N':
+                  rectJ3N.x = w * j;
+                  rectJ3N.y = h * i;
+                  rectJ3N.w = w;
+                  rectJ3N.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ3N, NULL, &rectJ3N);
+                  break;
+                case 'E':
+                  rectJ3E.x = w * j;
+                  rectJ3E.y = h * i;
+                  rectJ3E.w = w;
+                  rectJ3E.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ3E, NULL, &rectJ3E);
+                  break;
+                case 'S':
+                  rectJ3S.x = w * j;
+                  rectJ3S.y = h * i;
+                  rectJ3S.w = w;
+                  rectJ3S.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ3S, NULL, &rectJ3S);
+                  break;
+                case 'W':
+                  rectJ3W.x = w * j;
+                  rectJ3W.y = h * i;
+                  rectJ3W.w = w;
+                  rectJ3W.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ3W, NULL, &rectJ3W);
+                  break;
+              }
+              break;
             case -4:                //joueur4
-            rectJ4.x = w * j;
-            rectJ4.y = h * i;
-            rectJ4.w = w;
-            rectJ4.h = h;
-            SDL_RenderCopy(renderer, bitmapJ4, NULL, &rectJ4);
+              switch(j4.direction){
+                case 'N':
+                  rectJ4N.x = w * j;
+                  rectJ4N.y = h * i;
+                  rectJ4N.w = w;
+                  rectJ4N.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ4N, NULL, &rectJ4N);
+                  break;
+                case 'E':
+                  rectJ4E.x = w * j;
+                  rectJ4E.y = h * i;
+                  rectJ4E.w = w;
+                  rectJ4E.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ4E, NULL, &rectJ4E);
+                  break;
+                case 'S':
+                  rectJ4S.x = w * j;
+                  rectJ4S.y = h * i;
+                  rectJ4S.w = w;
+                  rectJ4S.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ4S, NULL, &rectJ4S);
+                  break;
+                case 'W':
+                  rectJ4W.x = w * j;
+                  rectJ4W.y = h * i;
+                  rectJ4W.w = w;
+                  rectJ4W.h = h;
+                  SDL_RenderCopy(renderer, bitmapJ4W, NULL, &rectJ4W);
+                  break;
+              }
             break;
             case 7:
             rectM1.x = w * j;
             rectM1.y = h * i;
             rectM1.w = w;
             rectM1.h = h;
-            SDL_RenderCopy(renderer, bitmapJ4, NULL, &rectM1);
+            SDL_RenderCopy(renderer, bitmapM1, NULL, &rectM1);
             break;
             case -11:
             bombeJ1.x = w * j;
@@ -344,10 +451,30 @@ void afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][
   SDL_DestroyTexture(bitmapFond); //Fond
   SDL_DestroyTexture(bitmapMurI); //Mur Incassable
   SDL_DestroyTexture(bitmapMurC); //Mur MurCassable
-  SDL_DestroyTexture(bitmapJ1); // joueur1
-  SDL_DestroyTexture(bitmapJ2); // joueur2
-  SDL_DestroyTexture(bitmapJ3); // joueur3
-  SDL_DestroyTexture(bitmapJ4); // joueur4
+  // joueur1
+  SDL_DestroyTexture(bitmapJ1N);
+  SDL_DestroyTexture(bitmapJ1E);
+  SDL_DestroyTexture(bitmapJ1S);
+  SDL_DestroyTexture(bitmapJ1W);
+
+  // joueur2
+  SDL_DestroyTexture(bitmapJ2N);
+  SDL_DestroyTexture(bitmapJ2E);
+  SDL_DestroyTexture(bitmapJ2S);
+  SDL_DestroyTexture(bitmapJ2W);
+
+  // joueur3
+  SDL_DestroyTexture(bitmapJ3N);
+  SDL_DestroyTexture(bitmapJ3E);
+  SDL_DestroyTexture(bitmapJ3S);
+  SDL_DestroyTexture(bitmapJ3W);
+
+  // joueur4
+  SDL_DestroyTexture(bitmapJ4N);
+  SDL_DestroyTexture(bitmapJ4E);
+  SDL_DestroyTexture(bitmapJ4S);
+  SDL_DestroyTexture(bitmapJ4W);
+
   SDL_DestroyTexture(bitmapM1); // monstre petit
   SDL_DestroyTexture(bitmapBombeJ1);
   SDL_DestroyTexture(bitmapBombeJ2);

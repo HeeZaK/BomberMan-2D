6fe52a72508b2db8684eace5e837622c56dee459
SimonGeslain <77961659+SimonGeslain@users.noreply.github.com>
2021-04-18 16:46:58 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -41,23 +41,22 @@ int main(int argc, char ** argv){
   srand( time(NULL));
 
   int jeu[M][N];
+  joueur_t * gagnant;
 
-
-  bombe_t bombeNull = {-1,-1,0};
   bombe_t bombe_vide[9];
 
   //Initialise les 4 joueurs
-  joueur_t joueur1 = {-1, 1, 1, 'E', 0, 0, 1, 1, -11, bombe_vide};
-  joueur_t joueur2 = {-2, 9, 13, 'W', 0, 0, 1, 1, -12, bombe_vide};
-  joueur_t joueur3 = {-3, 9, 1, 'W', 0, 0, 1, 1, -13, bombe_vide};
-  joueur_t joueur4 = {-4, 1, 13, 'E',0, 0, 1, 1, -14, bombe_vide};
+  joueur_t joueur1 = {-1, 1, 1, 'E', 0, 0, 0, 1, 1, -11, bombe_vide};
+  joueur_t joueur2 = {-2, 9, 13, 'W', 0, 0, 0, 1, 1, -12, bombe_vide};
+  joueur_t joueur3 = {-3, 9, 1, 'E', 0, 0, 0, 1, 1, -13, bombe_vide};
+  joueur_t joueur4 = {-4, 1, 13, 'W',0, 0, 0, 1, 1, -14, bombe_vide};
 
   SDL_Init(SDL_INIT_VIDEO);
 
 
   innitMatrice(jeu);
   placerMurIncassable(jeu);
-  placerMurCassable(jeu);
+  placerMurCassable(jeu, 85);
 
   //Placer les joueurs sur la map
   jeu[joueur1.pos_x][joueur1.pos_y]=joueur1.id;
@@ -65,7 +64,15 @@ int main(int argc, char ** argv){
   jeu[joueur3.pos_x][joueur3.pos_y]=joueur3.id;
   jeu[joueur4.pos_x][joueur4.pos_y]=joueur4.id;
 
-  afficherMap(joueur1, joueur2, joueur3, joueur4, jeu);
+  gagnant = afficherMap(joueur1, joueur2, joueur3, joueur4, jeu);
+
+  //Faire écran de victoire
+  if(gagnant!=NULL){
+    printf("Victoire du joueur %d\n", (gagnant->id)*(-1));
+  }
+  else{
+    printf("Erreur! Personne n'a gagné\n");
+  }
 
 
   SDL_Quit();

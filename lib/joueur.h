58368919ffb2861e93aa7613df4b244ca6357a5e
SimonGeslain <77961659+SimonGeslain@users.noreply.github.com>
2021-04-18 16:46:13 +0200

    Update joueur.h

--- lib/joueur.h
+++ lib/joueur.h
@@ -7,7 +7,7 @@ typedef struct bombe_s{
   // Position X et Y de la bombe
   int pos_x;
   int pos_y;
-  // Timer a la quelle la bombe a ete placee. Egale a 0 si la bombe n´est pas placee
+  // Timer à laquelle la bombe a été placée. Egale a 0 si la bombe n´est pas placee
   int timer;
 }bombe_t;
 
@@ -23,14 +23,18 @@ typedef struct joueur_s{
   int vie;
   // Variable determinant la vitesse a la quelle le joueur bouge
   int vitesse;
+  // Variable déterminant si un mouvement est en cours
+  int timer;
   // Variable determinant le nombre de bombe maximal que le joueur peux placer, de 1 a 9
   int nb_bomb;
   // Variable determinant la puissance de chaque bombe
   int puissance;
   // Variable determinant l´id de la bombe du joueur par rapport a la matrice
   int id_bombe;
+
   // Variable contenant les informations des jusqu´a 9 bombes du joueur
   bombe_t bombe[9];
+
 }joueur_t;
 
 void effetPouvoir(joueur_t * j, int x, int y, int jeu[M][N]);
@@ -40,3 +44,5 @@ int checkCollision(joueur_t * j, int direction, int jeu[M][N]);
 void changerRotaJoueur(joueur_t * j);
 
 void deplacerJ(joueur_t * j, int direction ,int jeu[M][N]);
+
+joueur_t * verifGagnant(joueur_t * j1, joueur_t * j2, joueur_t * j3, joueur_t * j4, int jeu[M][N]);

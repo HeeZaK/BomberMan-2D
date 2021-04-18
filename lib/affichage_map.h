fcecaa5174d23461516b85cb1f1ce397349423b8
SimonGeslain <77961659+SimonGeslain@users.noreply.github.com>
2021-04-18 23:43:53 +0200

    Add files via upload

--- lib/affichage_map.h
+++ lib/affichage_map.h
@@ -1,4 +1,4 @@
 
 
 
-joueur_t *afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][N]);
+joueur_t * afficherMap(joueur_t j1, joueur_t j2, joueur_t j3, joueur_t j4, int jeu[M][N]);
---
+++ lib/menu.h
@@ -0,0 +1,10 @@
+
+int menu(void);
+
+int menuMultijoueur(void);
+
+int lancerPartieMulti(int nb_joueur);
+
+void afficheGagnant(int id_joueur);
+
+int menuSolo(void);

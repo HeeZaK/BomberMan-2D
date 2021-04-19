38057c054cbb7d50d813eddba7e924761a6927c1
Rémi Ilango <45428394+IlangoRemi@users.noreply.github.com>
2021-04-19 21:43:39 +0200

    Add files via upload

---
+++ lib/level.h
@@ -0,0 +1,3 @@
+int level1(void);
+int level2(void);
+int level3(void);
--- lib/monstre.h
+++ lib/monstre.h
@@ -1,5 +1,5 @@
 
-#include "../lib/matriceBombe.h"
+
 
 typedef struct monstre_s{
   //id des monstres : petit monstre : -5, monstre moyen: -6, grand monstre :  -7
@@ -11,12 +11,21 @@ typedef struct monstre_s{
   char direction;
   // nombre de vie des monstres
   int vie;
-  // vitesse des monstres
-  int vitesse;
+
 }monstre_t;
 
 // fonction pour deplacer les monstres
-void deplacerMonstre(monstre_t *m, char direction ,int jeu[M][N]);
 
 
-int checkCollisionM(monstre_t * m, int direction, int jeu[M][N]);
+
+
+void deplacerMonstre(joueur_t *j1, monstre_t *m, char direction ,int jeu[M][N]);
+
+void effetBombeMonstre(joueur_t * j1, monstre_t * m1, monstre_t * m2, monstre_t * m3, int x, int y, int jeu[M][N]);
+
+int checkCollisionM(joueur_t * j1, monstre_t * m, int direction, int jeu[M][N]);
+
+int afficherMapMonstre(joueur_t j1, monstre_t m1, monstre_t m2, monstre_t m3, int jeu[M][N]);
+
+//une bombe détruit dans les 4 directions les objets cassable (mur cassable, pouvoir au sol, joueur)
+void DetruireMonstre(joueur_t * j1, monstre_t * m1, monstre_t * m2, monstre_t * m3, int k, int jeu[M][N]);
